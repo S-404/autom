65d133e268bd8b5b5c46a2d8675dfbc8ee71ab31
S-404 <sudarev.andrey.i@gmail.com>
2023-02-28 11:04:33 +0300

    add store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,18 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit"
 import engineSlice from "./engineSlice";
-
+import productionSlice from "./productionSlice"
 
 
 const rootReducer = combineReducers({
-        engine: engineSlice
+        engine: engineSlice,
+        prod: productionSlice,
 })
 
 export  const store = configureStore({
-    reducer: rootReducer
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    }),
 })
+
+export type RootState = ReturnType<typeof rootReducer>
